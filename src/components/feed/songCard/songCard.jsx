c4451fb7524971614f4ef089cf7e2ecb10fe02fd
Luis Tovar <ltovaralc@gmail.com>
2024-09-30 21:29:21 -0600

    trends de spotify obtenidos

--- src/components/feed/songCard/songCard.jsx
+++ src/components/feed/songCard/songCard.jsx
@@ -1,16 +1,15 @@
-import "./songCard.css"
-import LogoMeow from "../../../imgs/LOGOmeowsic.png"
+import "./songCard.css";
 
-const SongCard = () => {
-    return(
-        <div className="songCard">
-            <img src={LogoMeow} className="song-thumbnail__songCard"/>
-            <div className="song-details__songCard">
-            <span className="song-name__songCard">Name</span>
-            <span className="song-artist__songCard">Artist</span>
-            </div>
-        </div>
-    )
-}
+const SongCard = ({ songName, artistName, imageUrl }) => {
+  return (
+    <div className="songCard">
+      <img src={imageUrl} className="song-thumbnail__songCard" alt={songName} />
+      <div className="song-details__songCard">
+        <span className="song-name__songCard">{songName}</span>
+        <span className="song-artist__songCard">{artistName}</span>
+      </div>
+    </div>
+  );
+};
 
-export default SongCard
+export default SongCard;

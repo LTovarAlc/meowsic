ca2a7dc58bd1659db5626ebd12c05c6db9487001
Luis Tovar <ltovaralc@gmail.com>
2024-10-13 14:08:15 -0600

    componente de header agregado, nueva estructura de aplicacion agregada

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Dashboard from "./components/dashboard/dashboard";
+import Header from "./header/header";
 import "./reset.css"
 
 function App() {
   return (
     <div className="meowsic__container">
+      <Header/>
       <Dashboard/>
     </div>
   );
--- src/components/dashboard/dashboard.jsx
+++ src/components/dashboard/dashboard.jsx
@@ -1,7 +1,5 @@
 import React, { useState } from "react";
 import "./dashboard.css";
-import meowsic from "../../imgs/meowsic.png";
-import Searcher from "../searcher/searcher";
 import Player from "../player/player";
 import Feed from "../feed/feed";
 
@@ -14,16 +12,9 @@ const Dashboard = () => {
 
   return (
     <div className="dashboard">
-      <div className="logo__container">
-        <img src={meowsic} alt="meowsicLogo" className="meowsicLogo" />
-      </div>
       <div className="player__container">
         <Player song={selectedSong} />
       </div>
-      <div className="searcher__container">
-        <Searcher />
-      </div>
-      <div className="history__container">history</div>
       <div className="feed__container">
         <Feed setSelectedSong={setSelectedSong} />
       </div>
